3815f37670722e4a70c263cbac20d1701b5bde8c
Takeru9016 <youthfox046@gmail.com>
2024-11-08 22:18:30 +0530

    fix: Address TypeScript linting issues by adding unused variable rules in redux and api files

--- src/state/api.ts
+++ src/state/api.ts
@@ -4,6 +4,7 @@ export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
   tagTypes: [],
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   endpoints: (build) => ({}),
 });
 
