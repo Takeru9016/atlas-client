1d4372b7940a7f506b0f9efa42d735fab48b8d39
Takeru <71886594+Takeru9016@users.noreply.github.com>
2024-11-24 07:00:46 +0000

    feat: Timeline Chart design completed

--- src/components/TimelineView.tsx
+++ src/components/TimelineView.tsx
@@ -1,5 +1,6 @@
 import { useMemo, useState } from "react";
-import { DisplayOption, ViewMode } from "gantt-task-react";
+import { DisplayOption, Gantt, ViewMode } from "gantt-task-react";
+import "gantt-task-react/dist/index.css";
 
 import { useAppSelector } from "@/app/redux";
 import { useGetTasksQuery } from "@/state/api";
@@ -33,14 +34,25 @@ export default function TimelineView({
       tasks?.map((task) => ({
         start: new Date(task.startDate as string),
         end: new Date(task.dueDate as string),
-        title: task.title,
+        name: task.title,
         id: `Task-${task.id}`,
-        type: "task" as "TaskType",
+        type: "task" as TaskTypeItems,
         progress: task.points ? (task.points / 10) * 100 : 0,
         isDisabled: false,
       })) || []
     );
   }, [tasks]);
+
+  const handleViewModeChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setDisplayOptions((prev) => {
+      return {
+        ...prev,
+        viewMode: event.target.value as ViewMode,
+      };
+    });
+  };
 
   if (isLoading) return <div>Loading...</div>;
 
@@ -51,4 +63,47 @@ export default function TimelineView({
         : error.message || "An unknown error occurred";
     return <div>{errorMessage}</div>;
   }
+
+  return (
+    <div className="px-4 xl:px-6">
+      <div className="flex flex-wrap items-center justify-between gap-2 py-5">
+        <h1 className="me-2 text-lg font-bold dark:text-white">
+          Project Task Timeline
+        </h1>
+        <div className="relative inline-block w-64">
+          <select
+            className="focus:shadow-outline hover:border-gray-500dark:border-dark-secondary block w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 pr-8 leading-tight shadow dark:bg-dark-secondary dark:text-white"
+            value={displayOptions.viewMode}
+            onChange={handleViewModeChange}
+          >
+            <option value={ViewMode.Day}>Day</option>
+            <option value={ViewMode.Week}>Week</option>
+            <option value={ViewMode.Month}>Month</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="overflow-hidden rounded-md bg-white shadow dark:bg-dark-secondary dark:text-white">
+        <div className="timeline">
+          <Gantt
+            tasks={ganttTasks}
+            {...displayOptions}
+            columnWidth={displayOptions.viewMode === ViewMode.Month ? 150 : 100}
+            listCellWidth="100px"
+            barBackgroundColor={isDarkMode ? "#101214" : "#aeb8c2"}
+            barBackgroundSelectedColor={isDarkMode ? "#000000" : "9ba1a6"}
+          />
+        </div>
+
+        <div className="px-4 pb-5 pt-1">
+          <button
+            className="flex items-center justify-center rounded-md border bg-blue-primary px-3 py-2 text-white hover:border-blue-600 dark:border-dark-secondary dark:hover:border-blue-400"
+            onClick={() => setIsModalNewTaskOpen(true)}
+          >
+            Add New Task
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
